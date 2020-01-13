e5726bd5bb45376e31c8ef615cfaffdf33833392
Adela Darmansyah and Andriy Veremyeyev <andriy@>
2020-01-13 10:43:59 -0800

    Add if else conditions

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,24 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+var question = prompt ("You would like to do add (1), substract (2), multiply (3) or divide (4)")
+
+if (question === "1") {
+  var num1 = parseInt(prompt("Enter a number:"));
+  var num2 = parseInt(prompt("Enter another number:"));
+  alert(add(num1, num2));
+} else if (question === "2") {
+  num1 = parseInt(prompt("Enter a number:"));
+  num2 = parseInt(prompt("Enter another number:"));
+  alert(substract(num1, num2));
+} else if (question === "3") {
+  num1 = parseInt(prompt("Enter a number:"));
+  num2 = parseInt(prompt("Enter another number:"));
+  alert(multiply(num1, num2));
+} else if (question === "4") {
+  num1 = parseInt(prompt("Enter a number:"));
+  num2 = parseInt(prompt("Enter another number:"));
+  alert(divide(num1, num2));
+} else {
+  alert ("You provided wrong input. Please, refresh the page")
+}    
